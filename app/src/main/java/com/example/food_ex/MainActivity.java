a5603d1b1999524c92258ff76ecce2521e7f97ec
TGDD DD <pakuganp@gmail.com>
2023-10-08 16:23:51 +0700

    làm giao diện main

--- app/src/main/java/com/example/food_ex/MainActivity.java
+++ app/src/main/java/com/example/food_ex/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.food_ex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.food_ex.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        Log.i("Activity: ", "onCreate");
+
+        loadData();
+
+    }
+    private void loadData(){
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Activity: ", "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Activity: ", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Activity: ", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Activity: ", "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Activity: ", "onResume");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
     }
 }
